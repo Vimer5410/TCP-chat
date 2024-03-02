bc7dfdee28e1b2ab5e5a29fa9959efe7546a559b
Vimer <gleb.dubinin@rambler.ru>
2024-03-03 02:30:43 +0300

    !

---
+++ TCP-chat/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MyNamespace
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip),port);
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpSocket.Bind(tcpEndPoint);
+            tcpSocket.Listen(5);
+            
+            
+        }
+    }
+}
+
