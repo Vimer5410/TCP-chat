3d91ab3e4818199e39e6953501b39ac2c6c2cf3a
Vimer <gleb.dubinin@rambler.ru>
2024-03-06 22:08:51 +0300

    Сделал фул сервер и клиент

--- TCP-chat/Program.cs
+++ TCP-chat/Program.cs
@@ -1,21 +1,32 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 namespace MyNamespace
 {
-    class Programm
+    class TCP_server
     {
         static void Main(string[] args)
         {
+            
             const string ip = "127.0.0.1";
             const int port = 8080;
             var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip),port);
             var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             tcpSocket.Bind(tcpEndPoint);
             tcpSocket.Listen(5);
-            
-            
+            var listener = tcpSocket.Accept();
+            var buffer = new byte[256];
+            var size = 0;
+            var data = new StringBuilder();
+            while (true)
+            {
+                size = listener.Receive(buffer);
+                data.Clear();
+                data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                Console.WriteLine(data.ToString());
+            }
         }
     }
 }
---
+++ TCP-client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Programm
+{
+    public class TCP_client
+    {
+        static void Main(string[] args)
+        { 
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+            var Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                Socket.Connect(ip,port);
+                while (Socket.Connected==true)
+                {
+                    var message = Console.ReadLine();
+                    var messageBytes = Encoding.UTF8.GetBytes(message);
+                    Socket.Send(messageBytes);
+                }
+                
+
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+            }
+
+        }
+    }
+}
