a7b8b30c89ac4d04e16eb970ba44b484197e9744
Vimer <gleb.dubinin@rambler.ru>
2024-03-11 22:57:14 +0300

    added async ability to send and receive messages

--- TCP-chat/Program.cs
+++ TCP-chat/Program.cs
@@ -2,30 +2,53 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MyNamespace
 {
     class TCP_server
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            
+
             const string ip = "127.0.0.1";
             const int port = 8080;
-            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip),port);
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
             var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
             tcpSocket.Bind(tcpEndPoint);
             tcpSocket.Listen(5);
-            var listener = tcpSocket.Accept();
+
+            var listener = await tcpSocket.AcceptAsync();
             var buffer = new byte[256];
             var size = 0;
             var data = new StringBuilder();
-            while (true)
+            
+            
+            try
             {
-                size = listener.Receive(buffer);
-                data.Clear();
-                data.Append(Encoding.UTF8.GetString(buffer, 0, size));
-                Console.WriteLine(data.ToString());
+                Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        size = await listener.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
+                        data.Clear();
+                        data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                        Console.WriteLine(data.ToString());
+                    }
+                });
+
+                while (true)
+                {
+                    var message = Console.ReadLine();
+                    var messageBytes = Encoding.UTF8.GetBytes(message);
+                    await listener.SendAsync(new ArraySegment<byte>(messageBytes), SocketFlags.None);
+                }
+
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex);
             }
         }
     }
--- TCP-client/Program.cs
+++ TCP-client/Program.cs
@@ -2,32 +2,47 @@
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Programm
 {
     public class TCP_client
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         { 
             const string ip = "127.0.0.1";
             const int port = 8080;
             var Socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var buffer = new byte[256];
+            var size = 0;
+            var data = new StringBuilder();
+            
+            
             try
             {
-                Socket.Connect(ip,port);
-                while (Socket.Connected==true)
+                await Socket.ConnectAsync(IPAddress.Parse(ip), port);
+
+                Task.Run(async () =>
+                {
+                    while (true)
+                    {
+                        var size = await Socket.ReceiveAsync(new ArraySegment<byte>(buffer), SocketFlags.None);
+                        data.Clear();
+                        data.Append(Encoding.UTF8.GetString(buffer, 0, size));
+                        Console.WriteLine(data.ToString());
+                    }
+                });
+
+                while (true)
                 {
                     var message = Console.ReadLine();
                     var messageBytes = Encoding.UTF8.GetBytes(message);
-                    Socket.Send(messageBytes);
+                    await Socket.SendAsync(new ArraySegment<byte>(messageBytes), SocketFlags.None);
                 }
-                
-
             }
             catch (SocketException ex)
             {
                 Console.WriteLine(ex.Message);
-                
             }
 
         }
